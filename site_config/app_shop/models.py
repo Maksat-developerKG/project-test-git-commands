efc7f9494b21519d0a78eaab840f706b5e969f4d
Maksat-developer <gangsta.okulu02@gmail.com>
2023-02-13 02:40:14 +0600

    add/Product/models.py

--- site_config/app_shop/models.py
+++ site_config/app_shop/models.py
@@ -12,3 +12,19 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="name in product:", max_length=100)
+
+
+    
+    class Meta:
+        verbose_name = ""
+        verbose_name_plural = ""
+
+
+
+    def __str__(self) -> str:
+        return self.name
+
